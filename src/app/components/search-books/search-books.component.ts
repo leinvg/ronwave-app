438790dcb4fcde3e3095f1834be6bf6b51266b6a
leincalu <alain.vg.96@gmail.com>
2025-02-22 17:57:35 -0500

    styles scroll bar and list items | filter data api | improve ux

--- src/app/components/search-books/search-books.component.ts
+++ src/app/components/search-books/search-books.component.ts
@@ -1,33 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { GoogleBooksService } from '../../services/google-books.service';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-search-books',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgStyle],
   templateUrl: './search-books.component.html',
   styleUrl: './search-books.component.css',
 })
 export class SearchBooksComponent {
   query = new FormControl('');
-  books: any[] = [];
+  bookList: any[] = [];
+  maxDropdownHeight: number = 88;
+  isDropdownOpen = false;
 
   constructor(private booksService: GoogleBooksService) {}
+
+  @HostListener('window:resize')
+  calculateDropdownHeight() {
+    const windowHeight = window.innerHeight;
+    const inputBottom =
+      document.getElementById('SearchBar')?.getBoundingClientRect().bottom || 0;
+    this.maxDropdownHeight = Math.max(88, windowHeight - inputBottom - 8 - 64);
+  }
+
+  onFocus() {
+    this.calculateDropdownHeight();
+    this.isDropdownOpen = true;
+  }
+
+  onBlur() {
+    this.isDropdownOpen = false;
+  }
 
   onSearch() {
     const queryValue = this.query.value;
     if (queryValue && queryValue.length > 2) {
       this.booksService.searchBooks(queryValue).subscribe((response) => {
-        this.books = response.items || [];
+        // this.bookList = response.items || [];
+        this.bookList = (response.items || []).filter(
+          (item: any) => item.volumeInfo.imageLinks?.thumbnail
+        );
       });
+    } else {
+      this.bookList = [];
     }
   }
 
-  selectBook(book: any): void {
+  selectBook(book: any) {
     console.log('Libro seleccionado:', book);
-  }
-
-  imWriting() {
-    console.log(this.query);
   }
 }
